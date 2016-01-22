4229dfe28af70d2f211592482932a143a4ddc94e
Theo Dedeken <Theo.Dedeken@UGent.be>
2016-01-22 18:14:37 +0100

    basic AI

--- Spaxel/src/code/entity/Enemy.java
+++ Spaxel/src/code/entity/Enemy.java
@@ -7,12 +7,18 @@ public class Enemy extends Entity{
 	private Sprite sprite;
 	private int health;
 	private boolean alive;
+	private double maxspeed;
+	private double acc;
+	private double xdir;
+	private double ydir;
 
 	public Enemy(double x, double y, double rot, int health, Sprite sprite) {
 		super(x, y, rot);
 		this.sprite = sprite;
 		this.health = health;
 		alive = true;
+		maxspeed = 10;
+		acc = 0.5;
 	}
 	
 	public boolean isAlive(){
@@ -24,7 +30,27 @@ public class Enemy extends Entity{
 			alive = false;
 		}
 		rot = Math.PI + Math.atan2(((double) (player.getX() - x)), (double) (player.getY() - y));
+		double dx = 0;
+		double dy = 0;
+		
+		dx = -Math.sin(rot) * acc;
+		dy = -Math.cos(rot) * acc;
+		if (controlSpeed(xdir + dx, ydir + dy)) {
+			xdir += dx;
+			ydir += dy;
+		} else {
+			xdir = 0.975 * xdir + dx;
+			ydir = 0.975 * ydir + dy;
+		}
+
+		x+=xdir;
+		y+=ydir;
 		super.update();		
+	}
+	
+	public boolean controlSpeed(double dx, double dy) {
+		double speed = Math.sqrt(dx * dx + dy * dy);
+		return speed < maxspeed;
 	}
 	
 	@Override
--- Spaxel/src/code/system/AISystem.java
+++ Spaxel/src/code/system/AISystem.java
@@ -2,6 +2,7 @@ package code.system;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import code.engine.Engine;
 import code.engine.EntityType;
@@ -22,17 +23,24 @@ public class AISystem extends GameSystem {
 	}
 	
 	public void update(){
+		Random rand = new Random();
 		List<Entity> enemies = engine.getEntityStream().getEntities(EntityType.ENEMY);
 		List<Entity> dead = new ArrayList<>();
-		Player player = (Player)engine.getEntityStream().getEntities(EntityType.PLAYER);
+		Player player = (Player)engine.getEntityStream().getEntities(EntityType.PLAYER).get(0);
 		for (Entity e : enemies){
-			e.update(player);
+			((Enemy)e).update(player);
 			if (!((Enemy)e).isAlive()){
 				dead.add(e);
 				engine.getEntityStream().addEntity(EntityType.SPAWNER, new ParticleSpawner(e.getX(), e.getY(), 10, 5, .5, 5, 300, new Sprite(6,6,2,0xffff0000)));
 			}
 		}
 		enemies.removeAll(dead);
+		if(enemies.size()<10){
+			Enemy e = new Enemy(player.getX() + rand.nextInt(256) - 128, player.getY() + rand.nextInt(256) - 128,0,50,engine.getSpriteAtlas().get("green"));
+			e.setHitShape(engine.getHitShapeAtlas().get("hitshape_green"));
+			e.update(player);
+			enemies.add(e);
+		}
 	}
 
 }

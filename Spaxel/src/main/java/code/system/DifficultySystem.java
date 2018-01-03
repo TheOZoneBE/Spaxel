55f5d43e94e00b045210008ee0d7108a6dfc8b99
Theo Dedeken <theo.dedeken@telenet.be>
2018-01-03 18:43:46 +0100

    variable spawrate and limit depending on time

--- Spaxel/src/main/java/code/system/DifficultySystem.java
+++ Spaxel/src/main/java/code/system/DifficultySystem.java
@@ -21,24 +21,35 @@ import java.util.Set;
  * Created by theo on 3/01/18.
  */
 public class DifficultySystem extends GameSystem {
+    private int nextSpawn = 0;
+    private int spawnCap = 2;
+    private String[] enemyIndustries = {"enemy_green_industry", "enemy_white_industry", "enemy_red_industry", "enemy_blue_industry"};
+    private Random rand;
+
     public DifficultySystem() {
         super(SystemType.DIFFICULTY);
+        rand = new Random();
     }
 
     public void update(){
-        Random rand = new Random();
+        if (nextSpawn >0){
+            nextSpawn--;
+        }
         Set<NEntity> enemies = Engine.getEngine().getNEntityStream().getEntities(EntityType.ENEMY);
         NEntity player = new ArrayList<>(Engine.getEngine().getNEntityStream().getEntities(EntityType.PLAYER)).get(0);
         PositionComponent playerPos = (PositionComponent)player.getComponent(ComponentType.POSITION);
 
-        if(enemies.size() < 1){
-            EnemyIndustry ei = (EnemyIndustry) Engine.getEngine().getIndustryMap().get("enemy_green_industry");
+        if(nextSpawn == 0 && enemies.size() < spawnCap){
+            EnemyIndustry ei = (EnemyIndustry) Engine.getEngine().getIndustryMap().get(enemyIndustries[rand.nextInt(4)]);
             NEntity entity = ei.produce(
                     new PositionComponent(new VectorF(playerPos.getCoord().getValue(0) + rand.nextInt(256) - 128, playerPos.getCoord().getValue(1) + rand.nextInt(256) - 128), 0));
             NEntity item = Engine.getEngine().getItems().produceRandom(prop -> prop.getType() == ItemType.PRIMARY);
             ((PrimaryComponent)entity.getComponent(ComponentType.PRIMARY)).addItem(item);
             item.addComponent(new LinkComponent(entity));
             Engine.getEngine().getNEntityStream().addEntity(entity);
+            nextSpawn = Engine.getEngine().getGameProperties().getGameTime() < 300 ? 350 - Engine.getEngine().getGameProperties().getGameTime() : 50;
+            int temp = Engine.getEngine().getGameProperties().getGameTime() / 120;
+            spawnCap = 2 + temp*temp;
         }
     }
 }
